09bf369e1d83b5939172ed7dc13b7071f4c84789
Ludovic Henry <ludovic@xamarin.com>
2016-01-19 18:27:42 +0000

    [runtime] Use MonoError for mono_object_isinst and mono_object_isinst_mbyref

--- mono/metadata/object.c
+++ mono/metadata/object.c
@@ -5351,13 +5351,23 @@ mono_object_unbox (MonoObject *obj)
 MonoObject *
 mono_object_isinst (MonoObject *obj, MonoClass *klass)
 {
+	MonoError error;
+	mono_object_isinst_checked (obj, klass, &error);
+	mono_error_raise_exception (&error);
+}
+
+MonoObject*
+mono_object_isinst_checked (MonoObject *obj, MonoClass *klass, MonoError *error)
+{
 	MONO_REQ_GC_UNSAFE_MODE;
+
+	mono_error_init (error);
 
 	if (!klass->inited)
 		mono_class_init (klass);
 
 	if (mono_class_is_marshalbyref (klass) || (klass->flags & TYPE_ATTRIBUTE_INTERFACE))
-		return mono_object_isinst_mbyref (obj, klass);
+		return mono_object_isinst_mbyref_checked (obj, klass, error);
 
 	if (!obj)
 		return NULL;
@@ -5368,9 +5378,19 @@ mono_object_isinst (MonoObject *obj, MonoClass *klass)
 MonoObject *
 mono_object_isinst_mbyref (MonoObject *obj, MonoClass *klass)
 {
+	MonoError error;
+	mono_object_isinst_mbyref_checked (obj, klass, &error);
+	mono_error_raise_exception (&error);
+}
+
+MonoObject*
+mono_object_isinst_mbyref_checked (MonoObject *obj, MonoClass *klass, MonoError *error)
+{
 	MONO_REQ_GC_UNSAFE_MODE;
 
 	MonoVTable *vt;
+
+	mono_error_init (error);
 
 	if (!obj)
 		return NULL;
@@ -5405,8 +5425,10 @@ mono_object_isinst_mbyref (MonoObject *obj, MonoClass *klass)
 		gpointer pa [2];
 
 		im = mono_class_get_method_from_name (rpklass, "CanCastTo", -1);
-		if (!im)
-			mono_raise_exception (mono_get_exception_not_supported ("Linked away."));
+		if (!im) {
+			mono_error_set_generic_error (error, "System", "NotSupportedException", "Linked away.");
+			return NULL;
+		}
 		im = mono_object_get_virtual_method (rp, im);
 		g_assert (im);
 	
