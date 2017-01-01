4fef1d289e707e42a42cf50c50037a58322a0540
Ludovic Henry <ludovic@xamarin.com>
2017-01-01 18:34:59 -0200

    [socket] Extract iocltsocket

--- mono/metadata/w32socket-win32.c
+++ mono/metadata/w32socket-win32.c
@@ -226,6 +226,19 @@ BOOL mono_w32socket_transmit_file (SOCKET hSocket, gpointer hFile, guint32 nNumb
 }
 #endif /* #if G_HAVE_API_SUPPORT(HAVE_CLASSIC_WINAPI_SUPPORT | HAVE_UWP_WINAPI_SUPPORT) */
 
+gint
+mono_w32socket_set_blocking (SOCKET socket, gboolean blocking)
+{
+	gulong nonblocking_long = !blocking;
+	return ioctlsocket (socket, FIONBIO, &nonblocking_long);
+}
+
+gint
+mono_w32socket_get_available (SOCKET socket, guint64 *amount)
+{
+	return ioctlsocket (sock, FIONREAD, (int*) amount);
+}
+
 void
 mono_w32socket_set_last_error (gint32 error)
 {
