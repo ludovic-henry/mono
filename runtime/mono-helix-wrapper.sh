953af99b223f7093f323a08eb03c7a66347074cb
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-07-24 01:33:00 +0200

    Add support for running nunit tests

--- runtime/mono-helix-wrapper.sh
+++ runtime/mono-helix-wrapper.sh
@@ -5,9 +5,17 @@ PATH="$r/runtime/_tmpinst/bin:$PATH"
 MONO_EXECUTABLE=${MONO_EXECUTABLE:-"$r/mono"}
 MONO_PATH="$r:$r/tests"
 export MONO_CFG_DIR MONO_PATH PATH
-chmod +x "${MONO_EXECUTABLE}"
 
 if [ "$1" = "run-bcl-tests" ]; then
-    export REMOTE_EXECUTOR="$r/RemoteExecutorConsoleApp.exe"
-    exec "${MONO_EXECUTABLE}" --config "$r/runtime/etc/mono/config" --debug xunit.console.exe "tests/$2" -noappdomain -noshadow -parallel none -xml testResults.xml -notrait category=failing -notrait category=nonmonotests -notrait Benchmark=true -notrait category=outerloop -notrait category=nonlinuxtests
+    if [ "$2" = "xunit" ]; then
+        export REMOTE_EXECUTOR="$r/RemoteExecutorConsoleApp.exe"
+        "${MONO_EXECUTABLE}" --config "$r/runtime/etc/mono/config" --debug xunit.console.exe "tests/$3" -noappdomain -noshadow -parallel none -xml testResults.xml -notrait category=failing -notrait category=nonmonotests -notrait Benchmark=true -notrait category=outerloop -notrait category=nonlinuxtests
+        exit $?
+    elif [ "$2" = "nunit" ]; then
+        MONO_REGISTRY_PATH="$HOME/.mono/registry" MONO_TESTS_IN_PROGRESS="yes" "${MONO_EXECUTABLE}" --config "$r/runtime/etc/mono/config" --debug nunit-lite-console.exe "tests/$3" -format:nunit2 -result:testResult.xml
+        exit $?
+    else
+        echo "Unknown test runner."
+        exit 1
+    fi
 fi
