8675c5b135a0384d672b68b2d049040a4060171c
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-07-23 13:26:23 +0200

    REMOTE_EXECUTOR

--- runtime/mono-helix-wrapper.sh
+++ runtime/mono-helix-wrapper.sh
@@ -8,5 +8,6 @@ export MONO_CFG_DIR MONO_PATH PATH
 chmod +x "${MONO_EXECUTABLE}"
 
 if [ "$1" = "run-bcl-tests" ]; then
+    export REMOTE_EXECUTOR="$r/RemoteExecutorConsoleApp.exe"
     exec "${MONO_EXECUTABLE}" --config "$r/runtime/etc/mono/config" --debug xunit.console.exe "tests/$2" -noappdomain -noshadow -parallel none -xml testResults.xml -notrait category=failing -notrait category=nonmonotests -notrait Benchmark=true -notrait category=outerloop -notrait category=nonlinuxtests
 fi
