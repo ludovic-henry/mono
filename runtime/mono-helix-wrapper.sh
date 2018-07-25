f627c5da6a99749242460098286437e0244edc12
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-07-26 00:22:22 +0200

    Fix custom xunit xml

--- runtime/mono-helix-wrapper.sh
+++ runtime/mono-helix-wrapper.sh
@@ -47,12 +47,12 @@ if [ "$1" = "run-verify" ]; then
             ok=false
         fi
     done;
-    if [ "$ok" = "false" ]; then
-        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='verify' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='0' failed='1' skipped='0' errors='0' time='0'><collection total='1' passed='0' failed='1' skipped='0' name='Test collection for verify' time='0'><test name='verify.all' type='verify' method='all' time='0' result='Fail'><failure exception-type='VerifyException'><message><![CDATA[Verifying framework assemblies failed. Check the log for more details.]]></message></failure></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
-        exit 1
-    else
+    if [ "$ok" = "true" ]; then
         echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='verify' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='1' failed='0' skipped='0' errors='0' time='0'><collection total='1' passed='1' failed='0' skipped='0' name='Test collection for verify' time='0'><test name='verify.all' type='verify' method='all' time='0' result='Pass'></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
         exit 0
+    else
+        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='verify' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='0' failed='1' skipped='0' errors='0' time='0'><collection total='1' passed='0' failed='1' skipped='0' name='Test collection for verify' time='0'><test name='verify.all' type='verify' method='all' time='0' result='Fail'><failure exception-type='VerifyException'><message><![CDATA[Verifying framework assemblies failed. Check the log for more details.]]></message></failure></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
+        exit 1
     fi
 fi
 
@@ -60,10 +60,10 @@ if [ "$1" = "run-compiler" ]; then
     cd tests/compiler
     MONO_PATH=".:$MONO_PATH" "${MONO_EXECUTABLE}" --config "$r/runtime/etc/mono/config" --verify-all compiler-tester.exe -mode:pos -files:v4 -compiler:"$r/mcs.exe" -issues:known-issues-net_4_x -log:net_4_x.log -il:ver-il-net_4_x.xml -compiler-options:"-d:NET_4_0;NET_4_5 -debug"
     if [ $? -eq 0 ]; then
-        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='compiler' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='1' failed='0' skipped='0' errors='0' time='0'><collection total='1' passed='1' failed='0' skipped='0' name='Test collection for compiler' time='0'><test name='compiler.all' type='verify' method='all' time='0' result='Pass'></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
+        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='compiler' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='1' failed='0' skipped='0' errors='0' time='0'><collection total='1' passed='1' failed='0' skipped='0' name='Test collection for compiler' time='0'><test name='compiler.all' type='compiler' method='all' time='0' result='Pass'></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
         exit 0
     else
-        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='compiler' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='0' failed='1' skipped='0' errors='0' time='0'><collection total='1' passed='0' failed='1' skipped='0' name='Test collection for compiler' time='0'><test name='compiler.all' type='verify' method='all' time='0' result='Fail'><failure exception-type='CompilerException'><message><![CDATA[Compiler tests failed. Check the log for more details.]]></message></failure>/test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
+        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='compiler' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='0' failed='1' skipped='0' errors='0' time='0'><collection total='1' passed='0' failed='1' skipped='0' name='Test collection for compiler' time='0'><test name='compiler.all' type='compiler' method='all' time='0' result='Fail'><failure exception-type='CompilerException'><message><![CDATA[Compiler tests failed. Check the log for more details.]]></message></failure></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
         exit 1
     fi
 fi
@@ -72,10 +72,10 @@ if [ "$1" = "run-compiler-errors" ]; then
     cd tests/compiler-errors
     MONO_PATH=".:$MONO_PATH" "${MONO_EXECUTABLE}" --config "$r/runtime/etc/mono/config" compiler-tester.exe -mode:neg -files:v4 -compiler:"$r/mcs.exe" -issues:known-issues-net_4_x -log:net_4_x.log -compiler-options:"-v --break-on-ice -d:NET_4_0;NET_4_5"
     if [ $? -eq 0 ]; then
-        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='compiler-errors' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='1' failed='0' skipped='0' errors='0' time='0'><collection total='1' passed='1' failed='0' skipped='0' name='Test collection for compiler-errors' time='0'><test name='compiler-errors.all' type='verify' method='all' time='0' result='Pass'></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
+        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='compiler-errors' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='1' failed='0' skipped='0' errors='0' time='0'><collection total='1' passed='1' failed='0' skipped='0' name='Test collection for compiler-errors' time='0'><test name='compiler-errors.all' type='compiler-errors' method='all' time='0' result='Pass'></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
         exit 0
     else
-        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='compiler-errors' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='0' failed='1' skipped='0' errors='0' time='0'><collection total='1' passed='0' failed='1' skipped='0' name='Test collection for compiler-errors' time='0'><test name='compiler-errors.all' type='verify' method='all' time='0' result='Fail'><failure exception-type='CompilerErrorsException'><message><![CDATA[Compiler error tests failed. Check the log for more details.]]></message></failure></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
+        echo "<?xml version='1.0' encoding='utf-8'?><assemblies><assembly name='compiler-errors' environment='Mono' test-framework='custom' run-date='$(date +%F)' run-time='$(date +%T)' total='1' passed='0' failed='1' skipped='0' errors='0' time='0'><collection total='1' passed='0' failed='1' skipped='0' name='Test collection for compiler-errors' time='0'><test name='compiler-errors.all' type='compiler-errors' method='all' time='0' result='Fail'><failure exception-type='CompilerErrorsException'><message><![CDATA[Compiler error tests failed. Check the log for more details.]]></message></failure></test></collection></assembly></assemblies>" > "${helix_root}/testResults.xml";
         exit 1
     fi
 fi
