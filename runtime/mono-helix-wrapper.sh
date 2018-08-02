4fd4ea5a0674b1c6cbbace1d710448d89298dcc7
Alexander Köplinger <alex.koeplinger@outlook.com>
2018-08-02 23:41:28 +0200

    chmod mprof-report

--- runtime/mono-helix-wrapper.sh
+++ runtime/mono-helix-wrapper.sh
@@ -226,6 +226,7 @@ if [ "$2" = "run-csi" ]; then
 fi
 
 if [ "$2" = "run-profiler" ]; then
+    chmod +x mprof-report
     cd tests/profiler || exit 1
     perl ptestrunner.pl "helix" xunit "${helix_root}/testResults.xml"
 fi
